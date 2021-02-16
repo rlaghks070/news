16ed29b8e55db2a4a57c1a811cef274ba47927b3
Woojoo <rlaghks070@naver.com>
2021-02-17 00:55:56 +0900

    Initial Commit

---
+++ news/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  news
+//
+//  Created by Whan Kim on 2021/02/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
